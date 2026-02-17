c30500ed1e8e68ee68c409b3bdabc0a4186aef09
Kai Brennecke <229121123+kai-brennecke@users.noreply.github.com>
2026-02-17 11:41:40 +0100

    simplify find_job_id

--- imagescripts/find_job_id.sh
+++ imagescripts/find_job_id.sh
@@ -3,7 +3,7 @@
 # This script needs to be sourced without arguments to work correctly
 # It will remove the first argument from the argument list if it is a number and saves it in the JOB_ID variable
 
-if [ ! -z "${1##*[!0-9]*}" ]; then
-  export JOB_ID=$1
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  export JOB_ID="$1"
   shift
 fi
